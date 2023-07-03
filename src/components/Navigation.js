62e38effd23c95bd3eefacd3bce99c501f30ebfb
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-07-03 15:58:33 +1000

    Updated Navigation component so it uses handlePageChange method passed as params from PortfolioContainer to click through different pages.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,54 @@
 import React from "react";
 
-const Navigation = () => {
+function Navigation({ currentPage, handlePageChange }) {
   return (
     <nav>
       <ul>
         <li>
-          <a href="#about">About Me</a>
+          <a
+            href="#about"
+            onClick={() => handlePageChange("About")}
+            className={currentPage === "About" ? "nav-link active" : "nav-link"}
+          >
+            About Me
+          </a>
         </li>
         <li>
-          <a href="#portfolio">Portfolio</a>
+          <a
+            href="#portfolio"
+            onClick={() => handlePageChange("Portfolio")}
+            className={
+              currentPage === "Portfolio" ? "nav-link active" : "nav-link"
+            }
+          >
+            Portfolio
+          </a>
         </li>
         <li>
-          <a href="#contact">Contact</a>
+          <a
+            href="#contact"
+            onClick={() => handlePageChange("Contact")}
+            className={
+              currentPage === "Contact" ? "nav-link active" : "nav-link"
+            }
+          >
+            Contact
+          </a>
         </li>
         <li>
-          <a href="#resume">Resume</a>
+          <a
+            href="#resume"
+            onClick={() => handlePageChange("Resume")}
+            className={
+              currentPage === "Resume" ? "nav-link active" : "nav-link"
+            }
+          >
+            Resume
+          </a>
         </li>
       </ul>
     </nav>
   );
-};
+}
 
 export default Navigation;
