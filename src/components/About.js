88c13e6f9e931f3908910dcd2ed0eae4c9b1559d
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-07-03 14:00:47 +1000

    Created the basic structure for About and Contact.

---
+++ src/components/About.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <br></br>
+      <p>
+        Voi here - full stack Web Developer with an extensive background in
+        real-time problem solving, intensive teamwork and management with a
+        focus on delivering client satisfaction down to the last detail.
+      </p>
+      <br></br>
+      <p>
+        I studied American Literature at Warsaw University and went on to work
+        as a Head Chef in high-pressure and fine dining kitchens across London.
+        With my combined love of problem solving and bringing complicated ideas
+        to life, I'm now in the brilliant world of web development.
+      </p>
+      <br></br>
+      <p>
+        I've collaborated on multiple projects, available within my portfolio,
+        including complex applications. I love the challenge of working both
+        independently and within a multi-disciplined team, and I really shine
+        when it comes to producing unique solutions that exceed expectations.
+      </p>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      <br></br>
+      <form>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input type="email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" rows="5" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+};
+
+export default Contact;
