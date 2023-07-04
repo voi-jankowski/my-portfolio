ffa1d3d2472a9ed30318f81f43ec94c7aee7a494
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-07-04 15:08:46 +1000

    Created image link for resume and removed cards from there and fixed the styling a bit.

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,72 +1,70 @@
 import React from "react";
-import { Container, Header, Card } from "semantic-ui-react";
+import { Container, Header, Image } from "semantic-ui-react";
 import "./Resume.css"; // Import custom CSS file for Resume component
 
 const Resume = () => {
   const resumeUrl =
-    "https://drive.google.com/file/d/13gJ0KhZifEDD9gqVvX2YrINbLB8X9t9a/view?usp=sharing";
+    "https://drive.google.com/file/d/1YLzP4XDYEDFxxemPhHGZjWhejxq0iCND/view?usp=sharing";
 
   return (
     <section id="resume">
       <Container>
         <Header as="h2">Resume</Header>
-        <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
-          View Resume
+
+        <a
+          href={resumeUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ui medium right floated image"
+        >
+          <img
+            src="assets\resume-screenshot.png"
+            alt="Resume"
+            className="resume-image"
+          />
         </a>
-        <Card.Group>
-          <Card className="proficiency-card">
-            <Card.Content>
-              <Card.Description>
-                <code>
-                  <span className="proficiency-header">
-                    Front-end Proficiencies
-                  </span>{" "}
-                  [
-                  <br />
-                  &nbsp;&nbsp;"HTML",
-                  <br />
-                  &nbsp;&nbsp;"CSS",
-                  <br />
-                  &nbsp;&nbsp;"JavaScript",
-                  <br />
-                  &nbsp;&nbsp;"JQuery",
-                  <br />
-                  &nbsp;&nbsp;"React",
-                  <br />
-                  &nbsp;&nbsp;"Bootstrap",
-                  <br />
-                  &nbsp;&nbsp;"Materialize"
-                  <br />]
-                </code>
-              </Card.Description>
-            </Card.Content>
-          </Card>
-          <Card className="proficiency-card">
-            <Card.Content>
-              <Card.Description>
-                <code>
-                  <span className="proficiency-header">
-                    Back-end Proficiencies
-                  </span>{" "}
-                  [
-                  <br />
-                  &nbsp;&nbsp;"APIs",
-                  <br />
-                  &nbsp;&nbsp;"Node",
-                  <br />
-                  &nbsp;&nbsp;"Express",
-                  <br />
-                  &nbsp;&nbsp;"MySQL, Sequelize",
-                  <br />
-                  &nbsp;&nbsp;"MongoDB, Mongoose",
-                  <br />
-                  &nbsp;&nbsp;"REST"
-                  <br />]
-                </code>
-              </Card.Description>
-            </Card.Content>
-          </Card>
-        </Card.Group>
+
+        <br />
+        <br />
+
+        <code>
+          <span className="proficiency-header">Front-end Proficiencies</span> [
+          <br />
+          &nbsp;&nbsp;"HTML",
+          <br />
+          &nbsp;&nbsp;"CSS",
+          <br />
+          &nbsp;&nbsp;"JavaScript",
+          <br />
+          &nbsp;&nbsp;"JQuery",
+          <br />
+          &nbsp;&nbsp;"React",
+          <br />
+          &nbsp;&nbsp;"Bootstrap",
+          <br />
+          &nbsp;&nbsp;"Materialize"
+          <br />]
+        </code>
+
+        <br />
+        <br />
+
+        <code>
+          <span className="proficiency-header">Back-end Proficiencies</span> [
+          <br />
+          &nbsp;&nbsp;"APIs",
+          <br />
+          &nbsp;&nbsp;"Node",
+          <br />
+          &nbsp;&nbsp;"Express",
+          <br />
+          &nbsp;&nbsp;"MySQL, Sequelize",
+          <br />
+          &nbsp;&nbsp;"MongoDB, Mongoose",
+          <br />
+          &nbsp;&nbsp;"REST"
+          <br />]
+        </code>
       </Container>
     </section>
   );
