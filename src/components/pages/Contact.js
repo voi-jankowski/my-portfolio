08a0883e6d980949e8e03ea8e7766472044550ab
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-07-04 12:50:15 +1000

    Created Container with Form for Contact page with basic styling.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,25 +1,29 @@
 import React from "react";
+import { Container, Form, Button } from "semantic-ui-react";
 
 const Contact = () => {
   return (
     <section id="contact">
-      <h2>Contact Me</h2>
-      <br></br>
-      <form>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" name="name" />
-        </div>
-        <div>
-          <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      <Container>
+        <h2>Contact Me</h2>
+        <br />
+        <Form>
+          <Form.Field required error="Please enter your name">
+            <label htmlFor="name">Name:</label>
+            <input type="text" name="name" placeholder="Name" />
+          </Form.Field>
+          <Form.Field required error="Please enter your email address">
+            <label htmlFor="email">Email address:</label>
+            <input type="email" name="email" placeholder="Email" />
+          </Form.Field>
+          <Form.Field required error="Please enter your message">
+            <label htmlFor="message">Message:</label>
+            <textarea name="message" rows="5" />
+          </Form.Field>
+
+          <Button type="submit">Submit</Button>
+        </Form>
+      </Container>
     </section>
   );
 };
