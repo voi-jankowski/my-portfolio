0648d8374df61baf41e5f94822ebb17bf3dc7a87
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-07-05 10:17:02 +1000

    Changed to LESS package for theming.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import "semantic-ui-css/semantic.min.css";
+// import "semantic-ui-css/semantic.min.css";
+import "semantic-ui-less/semantic.less";
 
 ReactDOM.render(
   <React.StrictMode>
