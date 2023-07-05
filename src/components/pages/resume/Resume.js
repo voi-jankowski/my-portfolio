528f03f52f0afde3782e6dd976d70e4cfe538c35
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-07-05 19:23:54 +1000

    Corrected the styling of Resume.

--- src/components/pages/resume/Resume.js
+++ src/components/pages/resume/Resume.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Header } from "semantic-ui-react";
+import { Container, Header, Grid } from "semantic-ui-react";
 import "./Resume.css"; // Import custom CSS file for Resume component
 
 const Resume = () => {
@@ -10,62 +10,77 @@ const Resume = () => {
     <Container id="resume">
       <br />
       <Header as="h2">Resume</Header>
+      <Grid stackable reversed="mobile" columns={2}>
+        <Grid.Column>
+          <Container id="code-container">
+            <br />
+            <code>
+              <span className="proficiency-header">
+                Front-end Proficiencies
+              </span>{" "}
+              [
+              <br />
+              &nbsp;&nbsp;<span className="custom-color">HTML</span>,
+              <br />
+              &nbsp;&nbsp;<span className="custom-color">CSS</span>,
+              <br />
+              &nbsp;&nbsp;<span className="custom-color">JavaScript</span>,
+              <br />
+              &nbsp;&nbsp;<span className="custom-color">JQuery</span>,
+              <br />
+              &nbsp;&nbsp;<span className="custom-color">React</span>,
+              <br />
+              &nbsp;&nbsp;<span className="custom-color">Bootstrap</span>,
+              <br />
+              &nbsp;&nbsp;<span className="custom-color">Materialize</span>
+              <br />]
+            </code>
 
-      <a
-        href={resumeUrl}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="ui medium right floated image"
-      >
-        <img
-          src="assets\resume-screenshot.png"
-          alt="Resume"
-          className="resume-image"
-        />
-        <span className="resume-overlay">Go to Resume</span>
-      </a>
+            <br />
+            <br />
 
-      <br />
-      <br />
+            <code>
+              <span className="proficiency-header">Back-end Proficiencies</span>{" "}
+              [
+              <br />
+              &nbsp;&nbsp;<span className="custom-color">APIs</span>,
+              <br />
+              &nbsp;&nbsp;<span className="custom-color">Node</span>,
+              <br />
+              &nbsp;&nbsp;<span className="custom-color">Express</span>,
+              <br />
+              &nbsp;&nbsp;<span className="custom-color">MySQL, Sequelize</span>
+              ,
+              <br />
+              &nbsp;&nbsp;
+              <span className="custom-color">MongoDB, Mongoose</span>,
+              <br />
+              &nbsp;&nbsp;<span className="custom-color">REST</span>
+              <br />]
+            </code>
+          </Container>
+        </Grid.Column>
+        <Grid.Column textAlign="center">
+          <br />
+          <br />
+          <a
+            href={resumeUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="ui medium image"
+          >
+            <img
+              src="assets\resume-screenshot.png"
+              alt="Resume"
+              className="resume-image"
+            />
+            <span className="resume-overlay">Go to Resume</span>
+          </a>
 
-      <code>
-        <span className="proficiency-header">Front-end Proficiencies</span> [
-        <br />
-        &nbsp;&nbsp;<span className="custom-color">HTML</span>,
-        <br />
-        &nbsp;&nbsp;<span className="custom-color">CSS</span>,
-        <br />
-        &nbsp;&nbsp;<span className="custom-color">JavaScript</span>,
-        <br />
-        &nbsp;&nbsp;<span className="custom-color">JQuery</span>,
-        <br />
-        &nbsp;&nbsp;<span className="custom-color">React</span>,
-        <br />
-        &nbsp;&nbsp;<span className="custom-color">Bootstrap</span>,
-        <br />
-        &nbsp;&nbsp;<span className="custom-color">Materialize</span>
-        <br />]
-      </code>
-
-      <br />
-      <br />
-
-      <code>
-        <span className="proficiency-header">Back-end Proficiencies</span> [
-        <br />
-        &nbsp;&nbsp;<span className="custom-color">APIs</span>,
-        <br />
-        &nbsp;&nbsp;<span className="custom-color">Node</span>,
-        <br />
-        &nbsp;&nbsp;<span className="custom-color">Express</span>,
-        <br />
-        &nbsp;&nbsp;<span className="custom-color">MySQL, Sequelize</span>,
-        <br />
-        &nbsp;&nbsp;<span className="custom-color">MongoDB, Mongoose</span>,
-        <br />
-        &nbsp;&nbsp;<span className="custom-color">REST</span>
-        <br />]
-      </code>
+          <br />
+          <br />
+        </Grid.Column>
+      </Grid>
     </Container>
   );
 };
