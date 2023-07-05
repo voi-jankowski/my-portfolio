af266cf3b657ce32ef212d1e45691742a94ffc73
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-07-05 14:56:09 +1000

    Changed the Cards into Items in Project.js.

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,25 +1,23 @@
 import React from "react";
-import { Card, Icon } from "semantic-ui-react";
+import { Icon, Item } from "semantic-ui-react";
 import "./Project.css"; // Import custom CSS file for Project component
 
 const Project = ({ title, image, deployedLink, githubLink }) => {
   return (
-    <Card className="project-card">
-      <div className="project-image">
-        <img src={image} alt={title} />
-      </div>
-      <Card.Content>
-        <Card.Header className="project-title">{title}</Card.Header>
-        <Card.Description className="project-links">
+    <Item>
+      <Item.Image size="medium" src={image} alt={title} />
+      <Item.Content>
+        <Item.Header className="project-title">{title}</Item.Header>
+        <Item.Description className="project-links">
           <a href={deployedLink} target="_blank" rel="noopener noreferrer">
             <Icon name="external alternate" size="large" />
           </a>
           <a href={githubLink} target="_blank" rel="noopener noreferrer">
             <Icon name="github" size="large" />
           </a>
-        </Card.Description>
-      </Card.Content>
-    </Card>
+        </Item.Description>
+      </Item.Content>
+    </Item>
   );
 };
 
