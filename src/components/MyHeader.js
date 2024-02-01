17b2cedf59f1a2220d273bbbe124af4e539a1450
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2024-02-02 10:23:27 +1100

    Changed the header to a logo and set it's style with reactive width.

--- src/components/MyHeader.js
+++ src/components/MyHeader.js
@@ -1,13 +1,11 @@
 import React from "react";
-import { Segment, Header } from "semantic-ui-react";
+import { Segment, Image } from "semantic-ui-react";
 import "../App.css"; // Import the styles.css file
 
 const MyHeader = () => {
   return (
     <Segment id="my-header" padded="very" vertical textAlign="center">
-      <Header as="h1">Voi Jankowski</Header>
-
-      <Header as="h3">Full Stack Web Developer</Header>
+      <Image src="/assets/Logo_White.png" alt="Voi Jankowski" centered />
     </Segment>
   );
 };
